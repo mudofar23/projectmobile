f6221396eb807ff484367b20a7e0159c1192f200
Your Name <dofarzx23@gmail.com>
2024-05-30 07:12:36 -0700

    Menambahkan file proyek layout mobile

---
+++ app/src/main/java/com/example/pinjambukuid/MenuBuku.kt
@@ -0,0 +1,20 @@
+package com.example.pinjambukuid
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MenuBuku : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_menu_buku)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
